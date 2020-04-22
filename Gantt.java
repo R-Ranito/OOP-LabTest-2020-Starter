b58d83482a92db47223aa2bc17ee621f9ace91f8
R-Ranito <55754781+R-Ranito@users.noreply.github.com>
2020-04-22 18:51:01 +0100

    Add files via upload
    Missing mouse interaction

---
+++ Gantt.java
@@ -0,0 +1,92 @@
+package ie.tudublin;
+
+import java.util.ArrayList;
+import processing.core.PApplet;
+import processing.data.Table;
+
+public class Gantt extends PApplet {
+    private float Graph = 100f;
+    private int GraphGap = 40;
+    private float GraphSize = width / 3f;
+    
+    
+
+    private ArrayList<Task> tasks;
+    // set the size of the window
+
+    public void settings() {
+        size(800, 600);
+    }
+
+
+    //load the csv file
+    public void loadTasks() {
+        Table list = loadTable("tasks.csv", "header");
+
+        list.rows().forEach(tableRow -> tasks.add(new Task(tableRow)));
+    }
+    
+    public void printTasks() {
+        tasks.forEach(System.out::println);
+    }
+
+    
+    //draw the graph
+    private void displayGraph() {
+        stroke(255);
+
+        for (int i = 1; i <= 30; i++) {
+            float position = map(i, 1, 30, Graph, width - GraphSize);
+
+            line(position, GraphSize, position, height - GraphSize);
+            fill(255);
+            text(i, position - 2, GraphSize / 2);
+        }
+    }
+
+
+
+    public void mousePressed()
+	{
+		println("Mouse pressed");	
+	}
+
+	public void mouseDragged()
+	{
+		println("Mouse dragged");
+	}
+
+//display the list in the graph and the color for each task in the list
+    public void displayTasks() {
+        for (int i = 0; i < tasks.size(); i++) {
+            Task task = tasks.get(i);
+
+            float Y = map(i, 0, tasks.size(), GraphSize * 2,height - GraphSize);
+
+            fill(255);
+            text(task.getTask(), GraphSize, Y);
+
+            noStroke();
+            fill(map(i, 0, tasks.size(), 0, 255), 255, 255);
+
+            float TaskStart = map(task.getStart(), 1, 30, Graph, width - GraphSize);
+            float TaskEnd = map(task.getEnd(), 1, 30, Graph, width - GraphSize);
+            rect(TaskStart, Y - (GraphGap / 2), TaskEnd - TaskStart, GraphGap, 5, 5, 5, 5);
+        }
+    }
+
+    public void setup() {
+        tasks = new ArrayList<>();
+        loadTasks();
+        printTasks();
+    }
+
+    public void draw() {
+
+        background(0);
+        colorMode(HSB);
+        displayGraph();
+        displayTasks();
+        
+    }
+}
---
+++ Task.java
@@ -0,0 +1,50 @@
+package ie.tudublin;
+
+import processing.data.TableRow;
+
+public class Task {
+    private String task;
+    private int start;
+    private int end;
+
+    public Task(String task, int start, int end) {
+        this.task = task;
+        this.start = start;
+        this.end = end;
+    }
+
+    public Task(TableRow tableRow) {
+        this(tableRow.getString("Task"), tableRow.getInt("Start"), tableRow.getInt("End"));
+    }
+
+    public String getTask() {
+        return task;
+    }
+
+    public void setTask(String task) {
+        this.task = task;
+    }
+
+    public int getStart() {
+        return start;
+    }
+
+    public void setStart(int start) {
+        this.start = start;
+    }
+
+    public int getEnd() {
+        return end;
+    }
+
+    public void setEnd(int end) {
+        this.end = end;
+    }
+
+    @Override
+    public String toString() {
+        return "Task='" + task + '\'' +
+            ", Start=" + start +
+            ", End=" + end;
+    }
+}
